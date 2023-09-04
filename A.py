dd46dac6e0ec330c51986f7459cd67d7c66758d4
babuush <thananapatpiyamassakul@gmail.com>
2023-09-04 16:33:17 +0700

    Add new files

---
+++ A.py
@@ -0,0 +1,16 @@
+import turtle
+turtle.Screen().bgcolor("orange")
+sk = turtle.Turtle()
+
+for i in range(9):
+   sk.fd(25)
+   sk.rt(90)
+   sk.fd(25)
+   sk.lt(90)
+sk.rt(180)
+sk.fd(227    )
+sk.rt(90)
+sk.fd(230)
+turtle.done()
+
+
