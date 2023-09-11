c7d15c2e38c8bdf92f163211c3966236437da831
babuush <thananapatpiyamassakul@gmail.com>
2023-09-11 14:29:08 +0700

    fea: change the turtl shape

--- stair-by-input.py
+++ stair-by-input.py
@@ -1,18 +1,19 @@
 import turtle
 turtle.Screen().bgcolor("orange")
-
 num = int(turtle.Screen().numinput("Insert number", "Steps"))
 length = int(turtle.Screen().numinput("Insert number", "Length of Steps"))
-sk = turtle.Turtle()
+turtle.shape("turtle")
+
+
 for i in range(num):
-   sk.fd(length)
-   sk.rt(90)
-   sk.fd(length)
-   sk.lt(90)
-sk.rt(180)
-sk.fd(length*num)
-sk.rt(90)
-sk.fd(length*num)
+   turtle.fd(length)
+   turtle.rt(90)
+   turtle.fd(length)
+   turtle.lt(90)
+turtle.rt(180)
+turtle.fd(length*num)
+turtle.rt(90)
+turtle.fd(length*num)
 
 turtle.done()
 
