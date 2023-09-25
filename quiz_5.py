a50de9432f10719d2d9fe4e1c460151965efda79
babuush <thananapatpiyamassakul@gmail.com>
2023-09-25 14:04:30 +0700

    Added quiz_5.py

---
+++ quiz_5.py
@@ -0,0 +1,20 @@
+import turtle
+p1=turtle.Turtle()
+sc=turtle.Screen()
+n = int(turtle.numinput('title', 'n'))
+cx = int(turtle.numinput('title', 'cx'))
+cy = int(turtle.numinput('title', 'cy'))
+p1.speed(100)
+p1.penup()  
+def write_at_click(x,y):      
+	p1.goto(x,y)
+	p1.pendown()
+	p1.dot(10, 'green')
+	p1.penup()
+	for i in range(1,4):
+		p1.goto(x,y-20*i)
+		p1.pendown()
+		p1.circle(20*i)
+		p1.penup()
+sc.onclick(write_at_click)
+sc.mainloop()
